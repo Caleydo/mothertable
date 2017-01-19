5f0ae62541546557e4ba48c582c648919d708568
Katarína Furmanová <xfurman@mail.muni.cz>
2017-01-19 09:59:25 +0100

    column resizing test

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ import {IDataType} from 'phovea_core/src/datatype';
 import {list as listData, convertTableToVectors} from 'phovea_core/src/data';
 import {choose} from 'phovea_ui/src/dialogs';
 import {create as createMultiForm} from 'phovea_core/src/multiform';
+import {createNode} from 'phovea_core/src/multiform/internal';
+import {IMultiForm} from 'phovea_core/src/multiform';
+
+
 
 /**
  * The main class for the App app
@@ -49,7 +53,9 @@ export class App {
   private addDataset(data: IDataType) {
     const parent = this.$node.select('main').append('div').classed('block', true).html(`<header class="toolbar"></header><main></main>`);
     const vis = createMultiForm(data, <HTMLElement>parent.select('main').node(), {});
-    vis.addIconVisChooser(<HTMLElement>parent.select('header').node());
+   // vis.addIconVisChooser(<HTMLElement>parent.select('header').node());
+    this.addIconVisChooser(<HTMLElement>parent.select('header').node(),vis);
+    vis.transform([2,2]);
   }
 
   /**
@@ -60,7 +66,38 @@ export class App {
     this.$node.select('.busy').classed('hidden', !isBusy);
   }
 
+  private addIconVisChooser(toolbar: HTMLElement, ...forms: IMultiForm[]) {
+  const s = toolbar.ownerDocument.createElement('div');
+  toolbar.insertBefore(s, toolbar.firstChild);
+  const visses = this.toAvailableVisses(forms);
+
+  visses.forEach((v) => {
+    let child = createNode(s, 'i');
+    v.iconify(child);
+    child.onclick = () => forms.forEach((f) => {
+      f.switchTo(v);
+      let sy:number = 600/f.size[0];
+      let sx:number = 200/f.size[1];
+     // f.size = [200,600];
+      f.transform([sx,sy]);
+    }) ;
+  });
 }
+
+private toAvailableVisses(forms: IMultiForm[]) {
+  if (forms.length === 0) {
+    return [];
+  }
+  if (forms.length === 1) {
+    return forms[0].visses;
+  }
+  //intersection of all
+  return forms[0].visses.filter((vis) => forms.every((f) => f.visses.indexOf(vis) >= 0));
+}
+
+}
+
+
 
 /**
  * Factory method to create a new app instance
