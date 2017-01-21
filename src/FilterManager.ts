7c486b8c74e8d4fb37b10aebe1b2d95420ca14c2
Bikram Kawan <bikramkawan@gmail.com>
2017-01-21 19:33:13 +0100

    filterManager testing

--- src/FilterManager.ts
+++ src/FilterManager.ts
@@ -61,17 +61,17 @@ export default class FilterManager {
           const uniqCat = dataVal.filter((x, i, a) => a.indexOf(x) === i);
           const dataInfo = {'name': name, value: uniqCat, type: dataType};
           console.log(divInfo, dataInfo, fid);
-          makeCategories(divInfo, dataInfo, fid);
+          makeCategories(divInfo, dataInfo);
         });
       } else if (dataType === 'int' || dataType === 'real') {
         (<any>data).data().then(function (dataVal) {
           const dataInfo = {'name': name, value: dataVal, type: dataType};
-          this.makeNumerical(divInfo, dataInfo);
+          makeNumerical(divInfo, dataInfo);
         });
       } else {
         (<any>data).data().then(function (dataVal) {
           const dataInfo = {'name': name, value: dataVal, type: dataType};
-          this.makeStringRect(divInfo, dataInfo);
+          makeStringRect(divInfo, dataInfo);
 
         });
       }
@@ -79,71 +79,20 @@ export default class FilterManager {
     } else if (vectorOrMatrix === 'matrix') {
       (<any>data).data().then(function (dataVal) {
         const dataInfo = {'name': name, value: dataVal[0], type: vectorOrMatrix, 'range': range};
-        this.makeMatrix(divInfo, dataInfo);
+        makeMatrix(divInfo, dataInfo);
       });
     }
 
-
   }
-
-
-  private makeNumerical(divInfo, dataInfo) {
-    const cellHeight = divInfo.filterRowHeight;
-    const divBlock = d3.select('.filterdialog').append('div')
-      .attr('data-uid', 'f' + divInfo.uid)
-      .style('display', 'flex')
-      .style('height', cellHeight + 'px')
-      .style('margin', '1px');
-    const div = divBlock.selectAll('div.numerical').data([dataInfo.name]).enter();
-    div.append('div')
-      .attr('class', 'numerical')
-      .text((d: any) => d);
-
-  }
-
-
-  private makeMatrix(divInfo, dataInfo) {
-    const cellHeight = divInfo.filterRowHeight;
-    const divBlock = d3.select('.filterdialog').append('div')
-      .attr('data-uid', 'f' + divInfo.uid)
-      .style('display', 'flex')
-      .style('height', cellHeight + 'px')
-      .style('margin', '1px');
-    const div = divBlock.selectAll('div.matrix').data(dataInfo.value).enter();
-    const colorScale = d3.scale.linear<string, number>().domain(dataInfo.range).range(['white', 'red']);
-    div.append('div')
-      .attr('class', 'matrix')
-      .style('background-color', colorScale);
-
-    const matrixName = divBlock.selectAll('div.matrixName').data([dataInfo.name]).enter();
-    matrixName.append('div')
-      .attr('class', 'matrixName')
-      .text((d) => d);
-
-  }
-
-
-  private makeStringRect(divInfo, dataInfo) {
-    const cellHeight = divInfo.filterRowHeight;
-    d3.select('.filterdialog').selectAll('div.' + dataInfo.name).data([dataInfo.name]).enter()
-      .append('div')
-      .classed(dataInfo.name, true)
-      .style('height', cellHeight + 'px')
-      .style('margin', '1px')
-      .style('border', '1px')
-      .text((d) => d);
-  }
-
 
 }
 
-function makeCategories(divInfo, dataInfo, uid) {
-  console.log(divInfo, dataInfo, uid)
+function makeCategories(divInfo, dataInfo) {
   const cellHeight = divInfo.filterRowHeight;
-  const catdiv = divInfo.div;
+  const filterDiv = divInfo.div;
   const c20 = d3.scale.category20();
-  const divBlock = catdiv.append('div')
-    .attr('data-uid', 'f' + divInfo.uid)
+  const divBlock = filterDiv.append('div')
+    .attr('data-uid', divInfo.uid)
     .style('display', 'flex')
     .style('margin', '1px')
     .style('height', cellHeight + 'px');
@@ -163,4 +112,56 @@ function makeCategories(divInfo, dataInfo, uid) {
   // });
 
   div.exit().remove();
+}
+
+
+function makeNumerical(divInfo, dataInfo) {
+  const cellHeight = divInfo.filterRowHeight;
+  const filterDiv = divInfo.div;
+  const divBlock = filterDiv.append('div')
+    .attr('data-uid', divInfo.uid)
+    .style('display', 'flex')
+    .style('height', cellHeight + 'px')
+    .style('margin', '1px');
+  const div = divBlock.selectAll('div.numerical').data([dataInfo.name]).enter();
+  div.append('div')
+    .attr('class', 'numerical')
+    .text((d: any) => d);
+
+}
+
+
+function makeMatrix(divInfo, dataInfo) {
+  const cellHeight = divInfo.filterRowHeight;
+  const filterDiv = divInfo.div;
+  const divBlock = filterDiv.append('div')
+    .attr('data-uid', divInfo.uid)
+    .style('display', 'flex')
+    .style('height', cellHeight + 'px')
+    .style('margin', '1px');
+  const div = divBlock.selectAll('div.matrix').data(dataInfo.value).enter();
+  const colorScale = d3.scale.linear<string, number>().domain(dataInfo.range).range(['white', 'red']);
+  div.append('div')
+    .attr('class', 'matrix')
+    .style('background-color', colorScale);
+
+  const matrixName = divBlock.selectAll('div.matrixName').data([dataInfo.name]).enter();
+  matrixName.append('div')
+    .attr('class', 'matrixName')
+    .text((d) => d);
+
+}
+
+
+function makeStringRect(divInfo, dataInfo) {
+  const cellHeight = divInfo.filterRowHeight;
+  const filterDiv = divInfo.div;
+  const divBlock = filterDiv.append('div');
+  divBlock.selectAll('div.' + dataInfo.name).data([dataInfo.name]).enter()
+    .append('div')
+    .classed(dataInfo.name, true)
+    .style('height', cellHeight + 'px')
+    .style('margin', '1px')
+    .style('border', '1px')
+    .text((d) => d);
 }
