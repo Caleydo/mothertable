8b4bfb483208f45694ccbc6ac56bb95bb9fe59fb
Bikram Kawan <bikramkawan@gmail.com>
2017-01-29 16:11:38 +0100

    non-categorical to non-categorical connection line #18

--- src/ConnectionLines.ts
+++ src/ConnectionLines.ts
@@ -38,6 +38,8 @@ export default class ConnectionLines {
           const previousData = previousBlock.data;
           const currentBlock = App.blockList.get(id);
           const currentData = currentBlock.data;
+          const previousDataType = (<any>previousData).desc.value.type;
+          const currentDataType = (<any>currentData).desc.value.type;
 
           previousData.data().then((previousValue) => {
 
@@ -45,28 +47,42 @@ export default class ConnectionLines {
 
               // console.log(currentValue, previousValue, currentValue[1], previousValue[1])
 
-              if ((<any>previousData).desc.value.type === 'string') {
+              if (previousDataType === 'string') {
 
-                console.log('yes');
+
                 const topPathData = new Map();
-                const stringName = (<any>previousData).desc.name;
                 const xpos = cellWidth / 2;
                 const ypos = cellHeight - cellHeight;
 
-                console.log(currentValue);
+                // console.log(currentValue);
                 previousValue.forEach((d, i) => {
-                  console.log(d, i);
-                  topPathData.set(d, {'x': xpos, 'y': ypos})
+                  //    console.log(d, i);
+                  topPathData.set(d, {'x': xpos, 'y': ypos});
                 });
 
                 const values = {previous: previousValue, current: currentValue};
                 const keys = {previous: previousKey, current: id};
                 const tableVector = {previous: previousData, current: currentData};
                 const cellData = {width: cellWidth, height: cellHeight};
-                categoricalLines(topPathData, values, tableVector, keys, cellData);
-              } else if ((<any>previousData).desc.value.type === 'categorical') {
 
-                console.log('hello cat');
+                if (currentData.desc.value.type === 'categorical') {
+
+                  categoricalLines(topPathData, values, tableVector, keys, cellData);
+                } else if (currentDataType === 'string' || currentDataType === 'real' || currentDataType === 'int') {
+
+                  console.log('single attribute');
+
+                  nonCategoricalLines(topPathData, values, tableVector, keys, cellData)
+
+                } else {
+
+                  return;
+                }
+
+
+              } else if (previousDataType === 'categorical') {
+
+
                 const categories = previousData.desc.value.categories;
                 const bottomCellDimension = cellWidth / categories.length;
                 console.log(bottomCellDimension)
@@ -77,7 +93,7 @@ export default class ConnectionLines {
                 divCatNames[0].forEach(function (d, i) {
                   const catName = d3.select(divCatNames[0][i]).datum();
                   const xpos = bottomCellDimension / 2 + i * bottomCellDimension;
-                  const ypos = cellHeight-cellHeight;
+                  const ypos = cellHeight - cellHeight;
                   topPathData.set(catName, {x: xpos, y: ypos});
 
                 });
@@ -86,8 +102,10 @@ export default class ConnectionLines {
                 const keys = {previous: previousKey, current: id};
                 const tableVector = {previous: previousData, current: currentData};
                 const cellData = {width: cellWidth, height: cellHeight};
-                categoricalLines(topPathData, values, tableVector, keys, cellData);
 
+                if (currentData.desc.value.type === 'categorical') {
+                  categoricalLines(topPathData, values, tableVector, keys, cellData);
+                }
               }
 
 
@@ -145,9 +163,8 @@ function categoricalLines(topPathData, values, tableVector, keys, cellData) {
     // console.log(currentValue[i]);
     const xposition = bottomPathData.get(values.current[i]).xpos;
     const yposition = bottomPathData.get(values.current[i]).ypos;
-    //  console.log(checkXposOverlap, d)
 
-    console.log(values.current[i], values.previous[i])
+    //console.log(values.current[i], values.previous[i])
     if (checkXposOverlap.has(values.current[i])) {
       const xpos = checkXposOverlap.get(values.current[i]).x;
       checkXposOverlap.set(values.current[i], {x: xpos + 10, y: yposition});
@@ -167,5 +184,56 @@ function categoricalLines(topPathData, values, tableVector, keys, cellData) {
     .attr('stroke', 'red')
     .attr('stroke-width', 1)
     .attr('fill', 'red');
+
+}
+
+function nonCategoricalLines(topPathData, values, tableVector, keys, cellData) {
+
+  console.log('noncategorical lines');
+
+  const data = values.previous;
+  console.log(data, values.current)
+  const bottomCellDimension = cellData.width / data.length;
+
+
+  const currentValues = values.current;
+  const bottomPathData = new Map();
+  currentValues.forEach(function (d, i) {
+    const name = d;
+    const xpos = 5 + i * bottomCellDimension;
+    const ypos = cellData.height;
+    bottomPathData.set(name, {'xpos': xpos, 'ypos': ypos});
+
+  });
+
+  // console.log(currentPathData)
+  const checkXposOverlap = new Map();
+  const lineDiv = d3.select(`[f-uid="${keys.previous}"]`).append('div').classed('lineConnection', true);
+  const svg = lineDiv.append('svg').attr('width', cellData.width)
+    .attr('height', cellData.height).selectAll('path').data(data)
+  svg.enter().append('path').attr('d', function (d, i) {
+
+    const xposition = bottomPathData.get(values.current[i]).xpos;
+    const yposition = bottomPathData.get(values.current[i]).ypos;
+
+    //console.log(values.current[i], values.previous[i])
+    if (checkXposOverlap.has(values.current[i])) {
+      const xpos = checkXposOverlap.get(values.current[i]).x;
+      checkXposOverlap.set(values.current[i], {x: xpos + 10, y: yposition});
+    } else {
+      checkXposOverlap.set(values.current[i], {x: xposition, y: yposition});
+
+    }
+    const bottomXpos = checkXposOverlap.get(values.current[i]).x;
+
+    const ypos = yposition;
+
+    return `M ${topPathData.get(values.previous[i]).x} ${topPathData.get(values.previous[i]).y} L ${bottomXpos} ${yposition}`;
+
+  })
+    .attr('stroke', 'red')
+    .attr('stroke-width', 1)
+    .attr('fill', 'red');
+
 
 }
