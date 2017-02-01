c78c3b20cab7d611d8c368e4b9053d9beeb1fb58
Bikram Kawan <bikramkawan@gmail.com>
2017-02-01 21:19:21 +0100

    slider icon  added and removed brushing#25

--- src/FilterManager.ts
+++ src/FilterManager.ts
@@ -8,7 +8,6 @@ import Block from './Block';
 import ConnectionLines from './ConnectionLines';
 import ascending = d3.ascending;
 
-
 export default class FilterManager {
 
   private filterUID = [];
@@ -228,18 +227,19 @@ function makeNumerical(divInfo, dataInfo, block, self) {
       .text((d: any) => d);
     block.filterDiv = divBlock;
     const svgDIV = div.append('svg')
-      .attr('height', cellHeight + 10)
+      .attr('height', cellHeight + 25)
       .attr('width', cellWidth);
     const binSize = dataInfo.bins;
 
     const cellDimension = cellWidth / binSize.length;
-    const colorScale = d3.scale.linear<string,number>().domain([0, d3.max(binSize)]).range(['white', 'red']);
+    const colorScale = d3.scale.linear<string,number>().domain([0, d3.max(binSize)]).range(['white', 'darkgrey']);
     const bidDivs = svgDIV.append('g').classed('binsEntries', true)
       .selectAll('g.bins').data(binSize).enter();
     bidDivs.append('rect').classed('bins', true)
       .attr('x', (d, i) => i * cellDimension)
       .attr('width', cellDimension)
       .attr('height', cellHeight)
+      .style('opacity', 1)
       .attr('fill', (d: any) => colorScale(d))
       .on('mouseover', function (d, i) {
         tooltipDiv.transition()
@@ -254,52 +254,159 @@ function makeNumerical(divInfo, dataInfo, block, self) {
           .duration(500)
           .style('opacity', 0);
       });
+    let iconAPos = 5;
+    let iconBPos = cellWidth - 5;
+    const leftRectBrush = svgDIV.append('rect').attr('x', 0)
+      .attr('width', cellWidth)
+      .attr('height', cellHeight)
+      .attr('visibility', 'hidden')
+      .attr('opacity', 1)
+      .attr('fill', '#666');
+
+    const rightRectBrush = svgDIV.append('rect').attr('x', 0)
+      .attr('width', cellWidth)
+      .attr('height', cellHeight)
+      .attr('visibility', 'hidden')
+      .attr('opacity', 0.2)
+      .attr('fill', '#666');
+    const lineA = svgDIV.append('path')
+      .classed('brushline', true)
+      .attr('d', `M${iconAPos} 0,L${iconAPos} ${cellHeight + 5}`)
+      .attr('stroke', 'black');
 
 
+    const lineB = svgDIV.append('path')
+      .classed('brushline', true)
+      .attr('d', `M${iconBPos} 0,L${iconBPos} ${cellHeight + 5}`)
+      .attr('stroke', 'black');
     //Adapted from https://bl.ocks.org/alexmacy/eb284831aff6f9d0119b
 
-    const scale = d3.scale.linear()
-      .domain(range)
-      .range([0, cellWidth]);
+    // const scale = d3.scale.linear()
+    //   .domain(range)
+    //   .range([5, cellWidth - 5]);
+    // const brush = d3.svg.brush().x(scale)
+    //   .extent(range);
 
-    const brush = d3.svg.brush()
-      .x(scale)
-      .extent(range);
+    // const brushg = svgDIV.select('g.binsEntries').append('g').classed('brush', true).call(brush);
+    // brushg.selectAll('rect')
+    //   .attr('height', cellHeight);
 
-    const brushg = svgDIV.select('g.binsEntries').append('g').classed('brush', true).call(brush);
+    const axisScale = d3.scale.linear().domain([iconAPos, iconBPos]).range(range);
 
-    brushg.selectAll('rect')
-      .attr('height', cellHeight);
     const textA = svgDIV.append('text').classed('leftVal', true)
+
       .attr('x', 0)
-      .attr('y', cellHeight + 10)
+      .attr('y', cellHeight + 25)
       .attr('font-family', 'sans-serif')
       .attr('font-size', '12px')
-      .attr('fill', 'red');
+      .attr('fill', 'black');
     const textB = svgDIV.append('text').classed('rightVal', true)
       .attr('x', cellWidth - 20)
-      .attr('y', cellHeight + 10)
+      .attr('y', cellHeight + 25)
       .attr('font-family', 'sans-serif')
       .attr('font-size', '10px')
-      .attr('fill', 'red');
+      .attr('fill', 'black');
 
-    let brushVal = (<any>brush).extent();
+    let brushVal = range;
+
+    const dragA = d3.behavior.drag()
+      .on('drag', function (d, i) {
+        const x = (<any>d3).event.x;
+        iconAPos = x;
+        if (x >= 5 && x <= cellWidth - 5) {
+          if ((iconBPos - iconAPos) > 20) {
+            console.log(iconAPos, iconBPos)
+            brushVal = [axisScale(iconAPos), axisScale(iconBPos)];
+            console.log(brushVal)
+            textA.attr('x', iconAPos)
+              .text(`${Math.floor(brushVal[0])}`);
+            textB.attr('x', iconBPos).text(`${Math.floor(brushVal[1])}`);
+
+            leftRectBrush.attr('width', iconAPos)
+              .attr('visibility', 'visible')
+              .attr('opacity', 0.8);
+
+            rightRectBrush.attr('x', iconBPos)
+              .attr('width', cellWidth - iconBPos)
+              .attr('visibility', 'visible')
+              .attr('opacity', 0.8);
+
+            lineA.attr('d', `M${iconAPos} 0,L${iconAPos} ${cellHeight + 5}`)
+            lineB.attr('d', `M${iconBPos} 0,L${iconBPos} ${cellHeight + 5}`)
+
+
+            const filterType = {numerical: brushVal};
+            self._rangeManager.onBrushNumerical(dataInfo.data, divInfo.uid, filterType);
+            d3.select(this).attr('transform', `translate(${iconAPos},${cellHeight + 10})`);
+          }
+
+        }
+      });
+
+    const dragB = d3.behavior.drag()
+      .on('drag', function (d, i) {
+        const x = (<any>d3).event.x;
+        iconBPos = x;
+        if (x >= 5 && x <= cellWidth - 5 && (iconBPos - iconAPos) > 20) {
+          console.log(iconAPos, iconBPos)
+          brushVal = [axisScale(iconAPos), axisScale(iconBPos)];
+          console.log(brushVal)
+          textA.attr('x', iconAPos)
+            .text(`${Math.floor(brushVal[0])}`);
+          textB.attr('x', iconBPos).text(`${Math.floor(brushVal[1])}`);
+
+          leftRectBrush.attr('width', iconAPos)
+            .attr('visibility', 'visible')
+            .attr('opacity', 0.8);
+
+          rightRectBrush.attr('x', iconBPos)
+            .attr('width', cellWidth - iconBPos)
+            .attr('visibility', 'visible')
+            .attr('opacity', 0.8);
+
+          lineA.attr('d', `M${iconAPos} 0,L${iconAPos} ${cellHeight + 5}`);
+          lineB.attr('d', `M${iconBPos} 0,L${iconBPos} ${cellHeight + 5}`);
+
+          const filterType = {numerical: brushVal};
+          self._rangeManager.onBrushNumerical(dataInfo.data, divInfo.uid, filterType);
+          d3.select(this).attr('transform', `translate(${iconBPos},${cellHeight + 10})`);
+        }
+      });
+
+
+    const triangleSymbol = d3.svg.symbol().type('triangle-up').size(100);
+    const iconA = svgDIV.append('path')
+      .classed('draggable', true)
+      .attr('d', triangleSymbol)
+      .attr('transform', `translate(${5},${cellHeight + 10})`)
+      .call(dragA)
+
+    const iconB = svgDIV.append('path')
+      .attr('d', triangleSymbol)
+      .attr('transform', `translate(${cellWidth - 5},${cellHeight + 10})`)
+      .call(dragB);
+
+
     textA.text(`${(<any>Math).floor(brushVal[0])}`);
     textB.text(`${(<any>Math).floor(brushVal[1])}`);
 
-    const scalePos = d3.scale.linear().domain(brushVal).range([0, cellWidth]);
 
-    brush.on('brush', function () {
-      brushVal = brush.extent();
+    // const scalePos = d3.scale.linear().domain(brushVal).range([5, cellWidth - 5]);
 
-      textA.attr('x', scalePos(brushVal[0]))
-        .text(`${(<any>Math).floor(brushVal[0])}`);
-      textB.attr('x', scalePos(brushVal[1]) - 20).text(`${(<any>Math).floor(brushVal[1])}`);
 
-      const filterType = {numerical: brush.extent()};
-      self._rangeManager.onBrushNumerical(dataInfo.data, divInfo.uid, filterType);
+    // brush.on('brush', function () {
+    //   console.log(iconAPos, iconBPos)
+    //   console.log(brush.extent())
+    //   brushVal = brush.extent();
+    //   textA.attr('x', scalePos(brushVal[0]))
+    //     .text(`${Math.floor(brushVal[0])}`);
+    //   textB.attr('x', scalePos(brushVal[1]) - 20).text(`${Math.floor(brushVal[1])}`);
+    //
+    //   iconA.attr('transform', `translate(${scalePos(brushVal[0])},${cellHeight + 10})`);
+    //   iconB.attr('transform', `translate(${scalePos(brushVal[1])},${cellHeight + 10})`);
 
-    });
+
+    // });
 
 
     FilterManager.filterList.set(divInfo.uid, self);
