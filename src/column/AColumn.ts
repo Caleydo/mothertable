6e0af3f715441a0db329199e387f5aab18e0d559
Miroslava Jaresova <miroslava.jaresova@gmail.com>
2017-02-17 11:46:01 +0100

    column locking width flexbox properties

--- src/column/AColumn.ts
+++ src/column/AColumn.ts
@@ -114,26 +114,31 @@ abstract class AColumn<T, DATATYPE extends IDataType> extends EventHandler {
     lockButton.addEventListener('click', () => {
         const b = d3.select(lockButton);
         if (b.classed('fa-lock')) {
-          // RELEASING WIDTH LOCK
-          this.fire(AColumn.EVENT_COLUMN_LOCK_CHANGED,"Lock release");
+          // UNLOCKING
+          b.attr('class', 'fa fa-unlock');
+          this.fire(AColumn.EVENT_COLUMN_LOCK_CHANGED,"unlocked");
+
           this.node.classList.add('itemWidth');
           this.node.classList.remove('itemFixedWidth');
-          this.node.style.width = String(this.node.clientWidth + 'px');
+          this.node.style.flex = "1 1 " + String(this.node.clientWidth + 'px');
+          //this.node.style.width = String(this.node.clientWidth + 'px');
           this.node.style.minWidth = String(this.minimumWidth + 'px');
           this.node.style.maxWidth = String(this.preferredWidth + 'px');
-          b.attr('class', 'fa fa-unlock');
+
         } else {
-          // LOCKING WIDTH
-          this.fire(AColumn.EVENT_COLUMN_LOCK_CHANGED,"Lock closed");
-          const currentWidth = String(this.node.clientWidth + 'px');
+          // LOCKING
+           b.attr('class', 'fa fa-lock');
+          this.fire(AColumn.EVENT_COLUMN_LOCK_CHANGED,"locked");
+
           this.node.classList.add('itemFixedWidth');
           this.node.classList.remove('itemWidth');
-          this.node.style.minWidth = currentWidth;
-          this.node.style.maxWidth = currentWidth;
-          //this.node.style.minWidth = null;
-          //this.node.style.width = null;
+          this.node.style.flex = "0 0 " + String(this.node.clientWidth + 'px');
+          /*const currentWidth = String(this.node.clientWidth + 'px');
+            this.node.style.minWidth = currentWidth;
+            this.node.style.maxWidth = currentWidth;
+            this.node.style.minWidth = null;
+            this.node.style.width = null; */
 
-          b.attr('class', 'fa fa-lock');
         }
       });
 }
