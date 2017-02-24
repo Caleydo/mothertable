f43ff04d2f0ac724efe3c553d697bf00b1fa6350
Bikram Kawan <bikramkawan@gmail.com>
2017-02-24 14:33:03 +0100

    added sort icon on vector data.

--- src/filter/AVectorFilter.ts
+++ src/filter/AVectorFilter.ts
@@ -4,7 +4,8 @@
 import AFilter from './AFilter';
 import {IVector} from 'phovea_core/src/vector';
 import {IStringValueTypeDesc} from 'phovea_core/src/datatype';
-
+import {SORT} from '../sortColumn/SortColumn';
+import * as d3 from 'd3';
 export declare type IStringVector = IVector<string, IStringValueTypeDesc>;
 
 export abstract class AVectorFilter<T, DATATYPE extends IVector<T, any>> extends AFilter<T, DATATYPE> {
@@ -18,9 +19,26 @@ export abstract class AVectorFilter<T, DATATYPE extends IVector<T, any>> extends
     node = document.createElement('div');
     ol.appendChild(node);
     node.classList.add('filter');
-
+    this.addSortIcon(node)
     return node;
   }
+
+
+  private addSortIcon(node) {
+    const sortIconNode = d3.select(node).append('div').classed('fa sort fa-sort-amount-asc', true);
+    sortIconNode.on('click', () => {
+      const b = sortIconNode.attr('class');
+      if (b === 'fa sort fa-sort-amount-asc') {
+        const sortMethod = SORT.desc;
+        sortIconNode.attr('class', 'fa sort fa-sort-amount-desc');
+      } else {
+        const sortMethod = SORT.asc;
+        sortIconNode.attr('class', 'fa sort fa-sort-amount-asc');
+      }
+    });
+
+  }
+
 
 }
 
