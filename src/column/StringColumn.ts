8ed3aeb4e99bfff16ea601e704d6f9fe706efee6
Bikram Kawan <bikramkawan@gmail.com>
2017-03-07 13:53:44 +0100

    switch string from table to list

--- src/column/StringColumn.ts
+++ src/column/StringColumn.ts
@@ -4,6 +4,8 @@
 
 import {AVectorColumn, IStringVector} from './AVectorColumn';
 import {EOrientation} from './AColumn';
+import {mixin} from 'phovea_core/src/index';
+import {IMultiFormOptions} from 'phovea_core/src/multiform';
 
 export default class StringColumn extends AVectorColumn<string, IStringVector> {
   minimumWidth: number = 80;
@@ -12,6 +14,12 @@ export default class StringColumn extends AVectorColumn<string, IStringVector> {
   constructor(data: IStringVector, orientation: EOrientation, parent: HTMLElement) {
     super(data, orientation);
     this.$node = this.build(parent);
+  }
+
+  protected multiFormParams($body: d3.Selection<any>): IMultiFormOptions {
+    return mixin(super.multiFormParams($body), {
+      initialVis: 'list'
+    });
   }
 
 }
