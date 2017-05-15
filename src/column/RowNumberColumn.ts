4f3efe1596d9c52f1ea03150841177eedf511ae2
Martin Ennemoser <martin.ennemoser@jku.at>
2017-05-15 17:18:29 +0200

    Changed creation order of aggregation and row column. Aggregation gets build first.

--- src/column/RowNumberColumn.ts
+++ src/column/RowNumberColumn.ts
@@ -5,10 +5,12 @@ import {IMotherTableType} from './ColumnManager';
 export default class RowNumberColumn extends AColumn<any, IDataType> {
 
   readonly minWidth: number = 10;
+  private readonly dataList: number[];
   private $div: d3.Selection<any>;
   constructor(data: any, orientation: EOrientation, $parent: d3.Selection<any>) {
     super(data, orientation);
     this.$node = this.build($parent);
+    this.dataList = [];
   }
 
   protected buildVertical($parent: d3.Selection<any>): d3.Selection<any> {
@@ -45,19 +47,17 @@ export default class RowNumberColumn extends AColumn<any, IDataType> {
     $blocks
       .style('height', heights[0] + 'px')
       .style('min-height', heights[0] + 'px');
-  }
 
-  updateRowNumberColumn(size : number[]) {
-    const list = [];
-    for (let i = 0; i <= size[0]; i++) {
-      list.push(i);
-    }
-
-    //const list = [3, 6, 78, 200, 3, 6, 78, 200, 3, 6, 78, 200];
-    this.$div.selectAll('div')
-      .data(list)
+      this.$div.selectAll('div')
+      .data(this.dataList)
       .enter()
       .append('div')
       .text(function(d){return d;});
+  }
+
+  updateRowNumberColumn(size : number[]) {
+    for (let i = 0; i <= size[0]; i++) {
+      this.dataList.push(i);
+    }
   }
 }
