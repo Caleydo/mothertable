035598d8aef5b55f8c4b97e2db7366274b17711b
Martin Ennemoser <martin.ennemoser@jku.at>
2017-06-07 12:10:28 +0200

    Code cleanup

--- src/filter/MatrixFilter.ts
+++ src/filter/MatrixFilter.ts
@@ -32,61 +32,6 @@ export default class MatrixFilter extends AFilter<number, INumericalMatrix> {
     this.addTrashIcon($toolbar);
     this.generateLabel($li);
     new DensityPlot(this.data, $li.select('main'), null, this.generateTooltip($li));
-    //this.generateMatrixHeatmap($li.select('main'));
-
     return $li;
   }
-
-
-  get filterDim(): {
-    width: number;
-    height: number
-  } {
-    this._filterDim = {width: 205, height: 35};
-    return this._filterDim;
-  }
-
-  set filterDim(value: {
-    width: number;
-    height: number
-  }) {
-    this._filterDim = value;
-  }
-
-  private generateRect($node: d3.Selection<any>) {
-
-    $node.append('div').classed('matrix', true);
-
-  }
-
-
-  private async generateMatrixHeatmap($node: d3.Selection<any>) {
-    const cellWidth = this.filterDim.width;
-    const histData = await this.getHistData();
-    const cellDimension = cellWidth / histData.length;
-    const colorScale = d3.scale.linear<string,number>().domain([0, cellWidth]).range(NUMERICAL_COLOR_MAP);
-    const binScale = d3.scale.linear()
-      .domain([0, d3.max(histData)]).range([0, this._filterDim.height]);
-    const $entries = $node.append('div').classed('matrixEntries', true);
-
-    const $list = $entries
-      .selectAll('div.matrixBins')
-      .data(histData).enter();
-
-    $list.append('div')
-      .attr('class', 'matrixBins')
-      .attr('title', (d, i) => `${i + 1}: ${d}`)
-      .style('height', (d, i) => binScale(d) + 'px')
-      .style('background-color', (d, i) => colorScale(cellDimension * i));
-  }
-
-  private async getHistData() {
-
-    const histData = await (<any>this.data).hist();
-    const bins = [];
-    histData.forEach((d, i) => bins.push(d));
-    return bins;
-
-  }
 }
-
