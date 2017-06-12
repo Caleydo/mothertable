f1f251973c41d58f2afe15a110cd6170e2e44e73
Bikram Kawan <bikramkawan@gmail.com>
2017-06-12 12:13:16 +0200

    fix the typo and only remove deaggregate event from single converted vector #398

--- src/column/NumberColumn.ts
+++ src/column/NumberColumn.ts
@@ -21,8 +21,8 @@ export default class NumberColumn extends AVectorColumn<number, INumericalVector
   projectedMatrix: boolean = false;
   matrixViewRange: Range;
 
-  private $points:d3.Selection<any>;
-  private $axis:d3.Selection<any>;
+  private $points: d3.Selection<any>;
+  private $axis: d3.Selection<any>;
   private scale: d3.scale.Linear<number, number> = d3.scale.linear();
   private axis: d3.svg.Axis = d3.svg.axis();
 
@@ -31,7 +31,7 @@ export default class NumberColumn extends AVectorColumn<number, INumericalVector
   constructor(data: INumericalVector, orientation: EOrientation, $parent: d3.Selection<any>, matrixCol?: MatrixColumn) {
     super(data, orientation);
     this.$node = this.build($parent);
-    this.attachListener();
+    //  this.attachListener();
   }
 
   protected buildToolbar($toolbar: d3.Selection<any>) {
@@ -60,8 +60,8 @@ export default class NumberColumn extends AVectorColumn<number, INumericalVector
     this.$axis.call(this.axis);
 
     const count = this.$axis.selectAll('.tick').size();
-    this.$axis.selectAll('.tick').each(function(data, i) {
-      if(i === count-1) {
+    this.$axis.selectAll('.tick').each(function (data, i) {
+      if (i === count - 1) {
         const tick = d3.select(this).select('text');
         tick.style('text-anchor', 'end');
       }
@@ -98,8 +98,8 @@ export default class NumberColumn extends AVectorColumn<number, INumericalVector
     this.$axis.call(this.axis);
   }
 
-  setFixedWidth(width:number) {
-    if(isNaN(width)) {
+  setFixedWidth(width: number) {
+    if (isNaN(width)) {
       return;
     }
     super.setFixedWidth(width);
@@ -134,13 +134,15 @@ export default class NumberColumn extends AVectorColumn<number, INumericalVector
   }
 
 
+  //Uncomment this if user want to activate the single converted vector from matrix and add the deaggregate matrix as independent column.
+  //See issue 398
   private attachListener() {
     if ((<any>this).data.m !== undefined) {
       // this.matrixViewRange = this.data.m.range.dim(1).asList();
       //  console.log(this.data.m, this.data.m.range.dim(1).asList())
       const $matrixChange = this.toolbar.select('.onHoverToolbar').insert('a', ':first-child')
-        .attr('title', 'Aggregated Me')
-        .html(`<i class="fa fa-exchange" aria-hidden="true"></i><span class="sr-only">Aggregate Me</span>`);
+        .attr('title', 'Deaggregated Me')
+        .html(`<i class="fa fa-exchange" aria-hidden="true"></i><span class="sr-only">Deaggregate Me</span>`);
 
       $matrixChange.on('click', () => {
         this.fire(NumberColumn.EVENT_CONVERT_TO_MATRIX, this);
