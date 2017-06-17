ff0525b494473ba1b4769a9b0748ffc2ebe98a8a
Martin Ennemoser <martin.ennemoser@jku.at>
2017-06-17 14:09:03 +0200

    Added ts-mockito lib and created unit tests

---
+++ tests/RightPanel.test.ts
@@ -0,0 +1,130 @@
+/**
+ * Created by Martin Ennemoser on 12.06.2017.
+ */
+/// <reference types="jasmine" />
+/// <reference types="ts-mockito" />
+import ColumnManager from '../src/column/ColumnManager';
+import {EOrientation} from '../src/column/AColumn';
+import IDType from '../../phovea_core/src/idtype/IDType';
+import * as d3 from 'd3';
+import {INumericalVector} from '../../phovea_core/src/vector/IVector';
+import TableVector from '../../phovea_core/src/table/internal/TableVector';
+import * as tsmockito from 'ts-mockito';
+import Table from '../../phovea_core/src/table/Table';
+import {
+  VALUE_TYPE_STRING, VALUE_TYPE_CATEGORICAL, VALUE_TYPE_INT, VALUE_TYPE_REAL,
+  IDataType
+} from 'phovea_core/src/datatype';
+import FilterManager from '../src/filter/FilterManager';
+import {ITableLoader, ITableLoader2, adapterOne2Two, viaAPI2Loader, viaDataLoader} from 'phovea_core/src/table/loader';
+import {IQueryArgs, ITableDataDescription, ITableColumn} from '../../phovea_core/src/table/ITable';
+import {Range, parse, range} from 'phovea_core/src/range';
+
+function createDummyLoader() {
+  const r: ITableLoader2 = {
+    rowIds: (desc: ITableDataDescription, range: Range) => {
+      return null;
+    },
+    rows: (desc: ITableDataDescription, range: Range) => {
+      return null;
+    },
+    objs: (desc: ITableDataDescription, range: Range) => {
+      return null;
+    },
+    data: (desc: ITableDataDescription, range: Range) => {
+      return null;
+    },
+    col: (desc: ITableDataDescription, column: string, range: Range) => {
+      return null;
+    },
+    view: (desc: ITableDataDescription, name: string, args: IQueryArgs) => {
+      return null;
+    }
+  };
+  return r;
+}
+
+function createDescriptionObject(colName : string) {
+  return  {
+        column: null,
+        name: colName,
+        desciption: null,
+        value: {
+          type: VALUE_TYPE_CATEGORICAL,
+          categories: []
+        }
+      };
+}
+
+function createTableVector(colName : string, cid : string) {
+  // example of a simple mock
+  //const tableMock:Table = tsmockito.mock(Table);
+  //tsmockito.when(tableMock.idtype).thenReturn(new IDType('dummy', 'name', 'dummies'));
+  //const table = tsmockito.instance(tableMock)
+
+  const dataDescription = {id: cid, idtype: 'dummy', size: [], columns: [], type: 'dummyType', name: 'dummyName', description: 'dummyDescription', fqname: 'fqname', ts: 5, creator: 'dummyCreator'};
+  const table = new Table(dataDescription, createDummyLoader());
+  return new TableVector<any, any>(table, -1, createDescriptionObject(colName));
+}
+
+async function pushNewColumn(colManager : ColumnManager, tableVector : TableVector<any, any>) {
+  await colManager.push(tableVector);
+}
+
+describe('ColumnManager', function() {
+  let colManager = null;
+  const dummyElement = document.createElement('div');
+  document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(dummyElement);
+  const l = d3.select(document.getElementById((' ')));
+  beforeEach(function() {
+    const mockIDType = tsmockito.mock(IDType);
+    colManager = new ColumnManager(mockIDType, EOrientation.Vertical, l);
+  });
+
+  it('adds two columns', function(done) {
+
+    let pr = pushNewColumn(colManager, createTableVector('aaa', 'id0'));
+    pr.then(function () {
+      expect(colManager.length).toBe(1);
+      pr = pushNewColumn(colManager, createTableVector('bbb', 'id01'));
+      return pr;
+    })
+    .then(function() {
+      expect(colManager.length).toBe(2);
+      done();
+    });
+  });
+
+  it('adds two identical columns', function(done) {
+    const tv0 = createTableVector('aaa', 'id0');
+    const tv1 = createTableVector('aaa', 'id0');
+    let pr = pushNewColumn(colManager, tv0);
+    pr.then(function () {
+      expect(colManager.length).toBe(1);
+      pr = pushNewColumn(colManager, tv1);
+      return pr;
+    })
+    .then(function() {
+      expect(colManager.length).toBe(2);
+      done();
+    });
+  });
+
+  it('remove two identical columns', function(done) {
+    const tv0 = createTableVector('aaa', 'id0');
+    const tv1 = createTableVector('aaa', 'id0');
+    let pr = pushNewColumn(colManager, tv0);
+    pr.then(function () {
+      pr = pushNewColumn(colManager, tv1);
+      return pr;
+    })
+    .then(function() {
+      expect(tv0.desc.id).toBe(tv1.desc.id);
+      expect(colManager.length).toBe(2);
+      colManager.remove(null, tv0);
+      expect(colManager.length).toBe(0);
+      done();
+      });
+  });
+
+});
